7d1ee956d8212994a78a8f95b01b1334bf2cc63f
Petersomond1 <peters_o_mond@yahoo.com>
2023-08-12 10:30:42 -0400

    first stage commit
    exit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 // Write your Color component here
+const Color = ({color}) => {
+  return <div className={color}></div>
+};
 
 const App = () => {
   return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
-  );
-};
+<div id="container">
+        <div id="navbar">
+              <div>Currently selected: </div>
+              <div className="red">red</div>
+          </div>
+
+            <div id="colors-list">
+               <Color color="red"/>
+              <Color color="blue"/>
+              <Color color="green"/>
+          </div>
+
+</div>
+  )};
 
 export default App;
